b3fc530439655cedf1aca158863684a524126088
yashi12671 <76646380+yashi12671@users.noreply.github.com>
2022-04-06 18:00:53 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,46 @@
+package dynamic;
+import java.util.*;
+
+public class fibonacci {
+	static int fibbo(int n,int[]dp)
+	{
+		if(n==0||n==1)
+		{
+			return n;
+		}
+		int ans1,ans2;
+		if(dp[n-1]==-1)
+		{
+			ans1 =fibbo(n-1,dp);
+			dp[n-1]=ans1;
+		}
+		else {
+			ans1 =dp[n-1];
+		}
+		if(dp[n-2]==-1)
+		{
+			ans2=fibbo(n-2,dp);
+			dp[n-2]=ans2;
+		}
+		else {
+			ans2=dp[n-2];
+		}
+		int myAns = ans1+ans2;
+		return myAns;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[]dp = new int[n+1];
+		for(int i=0;i<dp.length;i++)
+		{
+			dp[i]=-1;
+		}
+		int ans = fibbo(n,dp);
+		System.out.println(ans);
+		
+
+	}
+
+}
